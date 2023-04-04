6277d798039f9c5e63d3cb63255dafb3a522a6c8
Conveus <connorhutnell@yahoo.co.uk>
2023-04-04 18:52:00 +0100

    Added for loop

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,16 @@ public class Main {
         // There is a shortcut for this by typing sout
         System.out.println();
         // There are other shortcuts too, remember to look them up
+
+        // Using print instead of println will not move the cursor to a new line
+        System.out.print("First Name, ");
+        System.out.println(":Last Name");
+
+        // For loops work the same as in C++ and C#
+        for(int i = 0; i <= 10; i++)
+        {
+            // Can print other data types
+            System.out.println(i);
+        }
     }
 }
