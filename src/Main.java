df88c1b8502437c6c8699aec7d913d97b151af3d
Conveus <connorhutnell@yahoo.co.uk>
2023-04-04 18:37:47 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // There is a shortcut for this by typing sout
+        System.out.println();
+        // There are other shortcuts too, remember to look them up
+    }
+}
